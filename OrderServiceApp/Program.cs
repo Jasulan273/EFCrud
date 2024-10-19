1a5b34b051f0418fe2805d186a2979e285028e9b
Jasulan273 <54472410+Jasulan273@users.noreply.github.com>
2024-10-19 18:47:16 +0600

    ocelot

--- OrderServiceApp/Program.cs
+++ OrderServiceApp/Program.cs
@@ -2,28 +2,36 @@ using Microsoft.EntityFrameworkCore;
 using OrderServiceApp.Data;
 using OrderServiceApp.Controllers.Interfaces;
 using OrderServiceApp.Services; 
+//using Ocelot.DependencyInjection;
+//using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
-
+// Настройка контекста базы данных
 builder.Services.AddDbContext<OrderContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+// Регистрация сервиса заказов
 builder.Services.AddScoped<IOrderService, OrderService>();
 
+// Регистрация контроллеров и Ocelot
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+//builder.Services.AddOcelot();
 
 var app = builder.Build();
 
+// Настройка Swagger
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
+// Использование Ocelot
 app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
+//await app.UseOcelot(); // Добавьте это для использования Ocelot
 app.Run();
